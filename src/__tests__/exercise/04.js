3b764d51966c17126f0898216b65fa81e70d49a7
PReichetanz <33567019+PReichetanz@users.noreply.github.com>
2023-05-10 09:59:35 +0200

    finish exercise 4

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -5,23 +5,46 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
+import faker from 'faker'
 
-test('submitting the form calls onSubmit with username and password', () => {
+function buildLoginForm(overrides) {
+  return {
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+    ...overrides,
+  }
+}
+
+test('submitting the form calls onSubmit with username and password', async () => {
   // 🐨 create a variable called "submittedData" and a handleSubmit function that
   // accepts the data and assigns submittedData to the data that was submitted
   // 💰 if you need a hand, here's what the handleSubmit function should do:
   // const handleSubmit = data => (submittedData = data)
+
+  const handleSubmit = jest.fn()
   //
   // 🐨 render the login with your handleSubmit function as the onSubmit prop
+  render(<Login onSubmit={handleSubmit} />)
   //
+  const {username, password} = buildLoginForm({password: 'abc'})
   // 🐨 get the username and password fields via `getByLabelText`
+  const usernameInputField = screen.getByLabelText('Username')
+  const passwordInputField = screen.getByLabelText('Password')
   // 🐨 use `await userEvent.type...` to change the username and password fields to
   //    whatever you want
+  await userEvent.type(usernameInputField, username)
+  await userEvent.type(passwordInputField, password)
   //
   // 🐨 click on the button with the text "Submit"
+  const submitButton = screen.getByRole('button', {name: 'Submit'})
+  await userEvent.click(submitButton)
   //
   // assert that submittedData is correct
   // 💰 use `toEqual` from Jest: 📜 https://jestjs.io/docs/en/expect#toequalvalue
+  expect(handleSubmit).toHaveBeenCalledWith({
+    username,
+    password,
+  })
 })
 
 /*
