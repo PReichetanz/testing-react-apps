9ab09d19509b22529609cf0faca54ebf33fa7d26
PReichetanz <33567019+PReichetanz@users.noreply.github.com>
2023-05-10 09:21:56 +0200

    finish exercise 3

--- src/__tests__/exercise/03.js
+++ src/__tests__/exercise/03.js
@@ -3,19 +3,21 @@
 
 import * as React from 'react'
 // 🐨 add `screen` to the import here:
-import {render, fireEvent} from '@testing-library/react'
+import {render, screen} from '@testing-library/react'
 import Counter from '../../components/counter'
+import userEvent from '@testing-library/user-event'
 
-test('counter increments and decrements when the buttons are clicked', () => {
-  const {container} = render(<Counter />)
+test('counter increments and decrements when the buttons are clicked', async () => {
+  render(<Counter />)
+  const user = userEvent.setup()
   // 🐨 replace these with screen queries
   // 💰 you can use `getByText` for each of these (`getByRole` can work for the button too)
-  const [decrement, increment] = container.querySelectorAll('button')
-  const message = container.firstChild.querySelector('div')
+  const [decrement, increment] = screen.getAllByRole('button')
+  const message = screen.getByText(/Current count/i)
 
   expect(message).toHaveTextContent('Current count: 0')
-  fireEvent.click(increment)
+  await user.click(increment)
   expect(message).toHaveTextContent('Current count: 1')
-  fireEvent.click(decrement)
+  await user.click(decrement)
   expect(message).toHaveTextContent('Current count: 0')
 })
